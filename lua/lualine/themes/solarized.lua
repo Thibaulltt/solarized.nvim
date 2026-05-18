1fa8b1cf3978baacf86e239307440933f9da6b8f
Thibault de Villèle <25150288+Thibaulltt@users.noreply.github.com>
2026-05-18 17:48:33 +0200

    lualine: update theme to be more toned down

--- lua/lualine/themes/solarized.lua
+++ lua/lualine/themes/solarized.lua
@@ -2,36 +2,60 @@ local colors = require('solarized.colors')
 
 local solarized = {}
 
+local default_x = { fg = colors.text, bg = colors.bg }
+local default_y = { fg = colors.text, bg = colors.bg_hl }
+local default_z = { fg = colors.text, bg = colors.bg_hl, gui = 'bold' }
+
 solarized.normal = {
 	a = { fg = colors.fg, bg = colors.bg_hl, gui = 'bold' },
 	b = { fg = colors.text, bg = colors.bg_hl },
-	c = { fg = colors.text_alt, bg = colors.bg },
+	c = { fg = colors.text, bg = colors.bg },
+	x = default_x,
+	y = default_y,
+	z = default_z,
 }
 
 solarized.insert = {
-	a = { fg = colors.fg, bg = colors.bases.green, gui = 'bold' },
+	a = { fg = colors.highlight, bg = colors.bases.green, gui = 'bold' },
 	b = { fg = colors.text, bg = colors.bg_hl },
+	x = default_x,
+	y = default_y,
+	z = default_z,
 }
 
 solarized.visual = {
-	a = { fg = colors.fg, bg = colors.purple, gui = 'bold' },
+	a = { fg = colors.highlight, bg = colors.bases.violet, gui = 'bold' },
 	b = { fg = colors.text, bg = colors.bg_hl },
+	x = default_x,
+	y = default_y,
+	z = default_z,
 }
 
 solarized.replace = {
-	a = { fg = colors.fg, bg = colors.bases.red, gui = 'bold' },
+	a = { fg = colors.highlight, bg = colors.bases.red, gui = 'bold' },
 	b = { fg = colors.text, bg = colors.bg_hl },
+	x = default_x,
+	y = default_y,
+	z = default_z,
 }
 
 solarized.command = {
-	a = { fg = colors.fg, bg = colors.bases.yellow, gui = 'bold' },
+	a = { fg = colors.highlight, bg = colors.bases.yellow, gui = 'bold' },
 	b = { fg = colors.text, bg = colors.bg_hl },
+	x = default_x,
+	y = default_y,
+	z = default_z,
 }
 
+solarized.active = solarized.normal
+
 solarized.inactive = {
-  a = { fg = colors.disabled, bg = colors.bg_hl, gui = 'bold' },
-  b = { fg = colors.disabled, bg = colors.bg_hl },
-  c = { fg = colors.disabled, bg = colors.bg }
+	a = { fg = colors.disabled, bg = colors.bg, gui = 'bold' },
+	b = { fg = colors.disabled, bg = colors.bg },
+	c = { fg = colors.disabled, bg = colors.bg },
+	x = default_x,
+	y = default_y,
+	z = default_z,
 }
 
 return solarized
